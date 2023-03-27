74063e18131e86fda11c1befd798a9a16eee9038
Maks <mgrechuhin@gmail.com>
2023-03-27 10:53:44 +0300

    jackson date format via servlet context listener

--- src/test/java/io/m_arc/camunda/test/CamundaInstanceTestApp.java
+++ src/test/java/io/m_arc/camunda/test/CamundaInstanceTestApp.java
@@ -1,12 +1,28 @@
 package io.m_arc.camunda.test;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.camunda.bpm.engine.rest.CustomJacksonDateFormatListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
 public class CamundaInstanceTestApp {
     public static void main(String... args) {
         SpringApplication.run(CamundaInstanceTestApp.class, args);
+    }
+    @Bean
+    public ServletContextInitializer initializer() {
+        return new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.addListener(new CustomJacksonDateFormatListener());
+                servletContext.setInitParameter("org.camunda.bpm.engine.rest.jackson.dateFormat",
+                        "yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+            }
+        };
     }
 }
